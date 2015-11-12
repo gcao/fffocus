c9c9217b6bf8171e5b960f3684904b3a6f3729ca
Guoliang Cao <gcao99@gmail.com>
2015-11-11 21:26:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fffocus
+
+## Introduction
+
+This is a simple HTML page that tests how long you can focus on a boring task.
+
+[Try it here](#todo)
+
+## Tasks
+
+- [ ] Show duration and reset on restarting
+
+## Misc
+
+https://github.com/d6u/web-playground
+
---
+++ js.js
@@ -0,0 +1,115 @@
+var NUMBER_VISIBLE_TIME = 2000;
+var WAIT_BASE           = 5000;
+var WAIT_VARIANCE       = 4000;
+
+var startTime;
+var finished;
+var sessionLength;
+var generated     = [];
+var timeoutRef    = null;
+var numberElem    = document.querySelector('#number');
+var recordingElem = document.querySelector('textarea');
+var resultElem    = document.querySelector('#result');
+var scoreElem     = document.querySelector('#score');
+var durationElem  = document.querySelector('#duration');
+
+function start() {
+  stop();
+  finished                 = false;
+  startTime                = new Date();
+  generated                = [];
+  recordingElem.value      = "";
+  resultElem.style.display = "none";
+  recordingElem.focus();
+  updateNumber();
+}
+
+//function resume() {
+//  updateNumber();
+//}
+
+function stop() {
+  clearTimeout(timeoutRef);
+  numberElem.innerHTML = "";
+}
+
+function finish() {
+  numberElem.innerHTML = "";
+  checkResult();
+}
+
+function updateNumber() {
+  timeoutRef = setTimeout(function() {
+    if (finished) return;
+
+    var number = Math.floor(Math.random()*100);
+    // Avoid duplication
+    while (number === generated[generated.length - 1]) {
+      number = Math.floor(Math.random()*100);
+    }
+
+    generated.push(number);
+    numberElem.innerHTML = String(number);
+
+    timeoutRef = setTimeout(function() {
+      if (finished) return;
+
+      numberElem.innerHTML = "";
+      updateNumber();
+    }, NUMBER_VISIBLE_TIME);
+  }, Math.random()*WAIT_VARIANCE + WAIT_BASE);
+}
+
+function checkResult() {
+  var recorded = recordingElem.value.trim().split(/[\s,]/);
+  var matched = 0;
+  var indexInExpected = 0;
+
+  recorded.forEach(function(item, i) {
+    var number = Number(item);
+    if (number === generated[indexInExpected]) {
+      matched ++;
+      indexInExpected ++;
+    } else {
+      while (recorded.length - i < generated.length - indexInExpected) {
+        indexInExpected ++;
+        if (number === generated[indexInExpected]) {
+          matched ++;
+          break;
+        }
+      }
+    }
+  });
+
+  var score = Math.round((matched/generated.length)*1000)/10;
+
+  scoreElem.innerHTML = matched + "/" + generated.length + " = " + score + "%";
+  resultElem.style.display = "block";
+}
+
+function updateDuration() {
+  setInterval(function() {
+    if (!startTime) return;
+
+    if (finished) return;
+
+    var duration = new Date() - startTime;
+    if (duration > Number(document.querySelector('select').value)*60*1000) {
+      finished = true;
+      finish();
+      return;
+    }
+
+    var now = new Date();
+    var secs = Math.round((now - startTime) / 1000);
+    var mins = Math.floor(secs / 60);
+    secs -= mins * 60;
+
+    if (secs < 10) secs = "0" + secs;
+
+    durationElem.innerText = mins + ":" + secs;
+  }, 500);
+}
+
+updateDuration();
+
